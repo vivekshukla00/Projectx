c25bb7433bf710fa331b09d480da2b2bb1642f57
vivekshukla00 <41097392+vivekshukla00@users.noreply.github.com>
2018-08-14 15:39:22 +0530

    Add files via upload

---
+++ ember-cli-build.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = function(defaults) {
+  let app = new EmberApp(defaults, {
+    // Add options here
+    'ember-composable-helpers': {
+      only: ['range', 'pipe', 'inc'],
+    },
+    ace: {
+      themes: ['ambiance', 'chaos', 'monokai', 'solarized_dark'],
+      modes: ['javascript', 'c_cpp', 'python', 'java'],
+      workers: ['javascript'],
+      exts: ['language_tools']
+    }
+  });
+
+  // Use `app.import` to add additional libraries to the generated
+  // output files.
+  //
+  // If you need to use different assets in different
+  // environments, specify an object as the first parameter. That
+  // object's keys should be the environment name and the values
+  // should be the asset to use in that environment.
+  //
+  // If the library that you are including contains AMD or ES6
+  // modules that you would like to import into your application
+  // please specify an object with the list of modules as keys
+  // along with the exports of each module as its value.
+  app.import('./bower_components/hls.js/dist/hls.light.min.js')
+
+  return app.toTree();
+};
